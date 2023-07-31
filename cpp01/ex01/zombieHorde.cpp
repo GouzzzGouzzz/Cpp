6eb2d8236314feeded838ab70943a08dc944838d
GouzzzGouzzz <gouz@DESKTOP-TU6S07P.localdomain>
2023-07-31 16:06:02 +0200

    fix

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -3,6 +3,11 @@
 Zombie* zombieHorde( int N, std::string name )
 {
 	Zombie *horde;
+	if (N < 0)
+	{
+		std::cout << "Negative value isn't supported\n";
+		return NULL;
+	}
 	horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		horde[i].set_name(name);
