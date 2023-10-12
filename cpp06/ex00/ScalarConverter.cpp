2d331080c275ccddd5d3d2fb18e6018d1296880e
GouzzzGouzzz <gouz@DESKTOP-TU6S07P.localdomain>
2023-10-12 19:54:27 +0200

    cpp06 need some fix but better

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -27,7 +27,40 @@ ScalarConverter::~ScalarConverter()
 	std::cout << "ScalarConverter destructor called\n";
 }
 
+void toChar(std::string convert)
+{
+	char temp = static_cast<char>(convert[0]);
+	if (convert.length() != 1)
+		std::cout << "Char : Doesn't make sense !\n";
+	else if (temp < 32 || temp > 126)
+		std::cout << "Char: Non displayable\n";
+	else
+		std::cout << "Char: " << temp << "\n";
+}
+
+void toInt(double val)
+{
+	if (val > INT_MAX || val < INT_MIN)
+		std::cout << "Int: Overflow / Underflow\n";
+	else
+		std::cout << "Int: " << static_cast<int>(val) << "\n";
+}
+
+double fromChar(std::string convert)
+{
+	char temp = static_cast<char>(convert[0]);
+	return (static_cast<double>(temp));
+}
+
 void ScalarConverter::convert(std::string convert)
 {
+	double temp;
 
+	temp = strtod(convert.c_str(), NULL);
+	if (convert.find_first_of("0123456789") > convert.length())
+		temp = static_cast<double>(convert[0]);
+	toChar(convert);
+	toInt(temp);
+	std::cout << "Float: " << static_cast<float>(temp) <<
+	"\nDouble: " << temp << "\n";
 }
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,8 +1,7 @@
 #include "ScalarConverter.hpp"
-
 int main(int ac, char **ag)
 {
-	if (ac != 2)
+ 	if (ac != 2)
 	{
 		std::cout << "Need only one args\n";
 		return 0;
