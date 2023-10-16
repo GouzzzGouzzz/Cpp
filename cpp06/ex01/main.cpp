72c81ae94c0cf5c9563067d9d0a29362b7153a23
GouzzzGouzzz <nmorandi@1D2.42angouleme.fr>
2023-10-16 16:40:22 +0200

    serialized ???

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,8 +3,14 @@
 int main()
 {
 	Data test("truc", 10);
+	std::cout << "Truc address: " << &test << "\n";
 	Serialize truc;
-	Data *ptr;
+	Data *ptr2;
 	test.getInfo();
-	
+	uintptr_t seri = truc.serialize(&test);
+	ptr2 = truc.deserialize(seri);
+	std::cout << "Deserialized address: " << &ptr2 << "\n";
+	ptr2->getInfo();
+	if (&(*ptr2) == &test)
+		std::cout << "the same\n";
 }
