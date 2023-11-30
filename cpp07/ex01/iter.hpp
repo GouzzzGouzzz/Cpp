6b3775d19a6d79da5e006d45cb14f8ff0d4d2fa7
GouzzzGouzzz <gouz@DESKTOP-TU6S07P.localdomain>
2023-11-30 15:41:22 +0100

    ex01 07

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,7 +1,13 @@
+#include <iostream>
 template <typename T>
-void iter(T addr[], T& len, T *func)
+void iter(T *array, int len, void (*func)(T &))
 {
 	for (int i = 0; i < len; i++)
-		func(addr[i]);
+		func(array[i]);
 }
 
+template <typename T>
+void print(T &truc)
+{
+	std::cout << truc << " ";
+}
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,13 +1,12 @@
 #include "iter.hpp"
 
-void add(void *truc)
-{
-	(void)truc;
-}
-
 int main()
 {
 	int tab[] = {0,1,2,3,4,5,6,7,8,9};
 	int len = 10;
-	iter(tab,len, &add);
+	std::string tab2[] = {"This", "is", "an", "array", "of","strings"};
+	iter(tab,len, &print);
+	std::cout << std::endl;
+	iter(tab2,6, &print);
+	std::cout << std::endl;
 }
