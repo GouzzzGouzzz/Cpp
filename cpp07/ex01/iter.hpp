93ece6beb30dbe60e7ce54664f29b0bbdbcd0344
GouzzzGouzzz <110063120+GouzzzGouzzz@users.noreply.github.com>
2023-12-18 13:03:11 +0100

    Update iter.hpp

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
-template <typename T>
-void iter(T *array, int len, void (*func)(T &))
+template <typename T> //template <typename T, typename FUNCTION>
+void iter(T *array, int len, void (*func)(T &)) //void iter(T *array, int len, FUNCTION func) 
 {
 	for (int i = 0; i < len; i++)
 		func(array[i]);
